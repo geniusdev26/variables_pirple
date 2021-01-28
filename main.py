95fb58bcd3d16cd2c23d1c0531a9e9732c83f0ad
Jeffrey Wainaina <68434262+geniusdev26@users.noreply.github.com>
2021-01-28 23:29:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import datetime
+"""
+It's My Life Bon Jovi is my most favorite song. It helped me realize that my life
+solely depends on me. I am a free soul. Take an example of the lyrics, 'My Heart is like an open
+higway, like Frankie said I did it my way..." I can be a python guru!!!!
+"""
+#Song Metadata
+Genre = 'Rock (Classic Rock)'
+AlbumTitle = "Crush"
+SongName = "It's My Life"
+SongID = 4531211222
+Artist = 'Bon Jovi Band'
+SongWriters = "Jon Bon Jovi, Richie Sambora, Max Martin"
+SongProducers = "Luke Ebbin, Max Martin, Rami Yacoub"
+SongMood = "Excited, Free-Soul"
+SongDurationInSeconds = 264
+
+#Number of veiws on YouTube
+NumberOfViews = "943,639,989"
+
+#Date of Release yyyy/mm/dd/
+ReleaseDate = datetime.date(2000, 5, 23)
+
+print(Genre)
+print(AlbumTitle)
+print(SongName)
+print(SongID)
+print(Artist)
+print(SongWriters)
+print(SongProducers)
+print(SongMood)
+print(SongDurationInSeconds)
+print(NumberOfViews)
+print(ReleaseDate)
