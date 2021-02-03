976d8348fa79b38cbc16c69a2a71e9b98fedea19
Jeffrey Wainaina <68434262+geniusdev26@users.noreply.github.com>
2021-02-03 22:46:03 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,35 @@
+"""
+Function Structure
+def FunctionName (input):
+    Action
+    return Output
+"""
+def genre ():
+    print("Rock (Classic Rock)")
+
+
+def songID ():
+    print(4531211222)
+
+
+def songDuration ():
+    print (264) #displaying duration in seconds
+
+
+def songMood ():
+    songMoodType = "Excited"
+    print(songMoodType)
+    if songMoodType == "Excited":
+        print(True)
+    else:
+        print(False)
+
+
+
+
+genre()
+songID()
+songDuration()
+songMood()
+
+    
